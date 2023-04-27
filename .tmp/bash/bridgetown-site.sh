4980fdbc2f74faa78f63c3af28a506e54694ee7a
Richard Joubert <rjoubert@nsd.team>
2023-04-26 20:11:30 -0400

    update
    Signed-off-by: Richard Joubert <rjoubert@nsd.team>

--- .tmp/bash/bridgetown-site.sh
+++ .tmp/bash/bridgetown-site.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-alias btown="bin/bridgetown"
-alias bstart="bin/bridgetown start"
+alias btown="rbenv exec ruby bin/bridgetown"
+alias bstart="rbenv exec ruby bin/bridgetown start"
 alias brbundle="rbenv exec bundle"
 
+# brbundle add minitest-reporters-1.6.0 shoulda-4.0.0 rails-dom-testing-2.0.3 ansi-1.5.0 builder-3.2.4 ruby-progressbar-1.13.0 shoulda-context-2.0.0 shoulda-matchers-4.5.1
